f18ac32406fc2bfe18dfc74da09c1b7b8e10c8a0
Madvocato <madvocato@protonmail.com>
2025-07-05 21:47:13 +0300

    изменения: настроена админ-панель для моделей recipe и ingridients

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Diet, Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1 # По умолчанию показывать 1 пустую строку для добавления
+
+class RecipeAdmin(admin.ModelAdmin):
+    # Добавляем инлайн-редактирование ингредиентов
+    inlines = [RecipeIngredientInline]
+    # Улучшаем отображение списка рецептов
+    list_display = ('name', 'cooking_time', 'meal_type')
+    # Добавляем фильтры
+    list_filter = ('meal_type', 'diets')
+    # Добавляем поиск
+    search_fields = ('name', 'description')
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'calories', 'proteins', 'fats', 'carbs')
+    search_fields = ('name',)
+
+admin.site.register(Diet)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
